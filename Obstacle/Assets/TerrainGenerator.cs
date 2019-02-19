6f8383787e1c24ad728f21ddb1b70c0eba247626
WaltWe <walter.weiffenbach@stemhigh.org>
2019-02-19 13:53:17 -0800

    staf

---
+++ Obstacle/Assets/TerrainGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject[] terrain;
+    private float terrPos = 0;
+    private GameObject[] activeTerrain;
+    public int viewDis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeTerrain = new GameObject[viewDis];
+        for (int i = 0; i < viewDis; i++)
+        {
+            int terrID = Random.Range(0, terrain.Length - 1);
+            GameObject terrainPiece = Instantiate(terrain[terrID], new Vector3(0, 0, terrPos), Quaternion.identity);
+            activeTerrain[i] = terrainPiece;
+            terrPos += terrainPiece.GetComponent<Renderer>().bounds.size.z;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Obstacle/Assets/scorekeeping.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scorekeeping : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.GetComponent<Transform>().position.z < 100)
+        {
+            GetComponent<Text>().text = "Score: " + ((int)player.GetComponent<Transform>().position.z / 10) % 10;
+        }
+        else
+        {
+            GetComponent<Text>().text = "You Win!";
+        }
+    }
+}
