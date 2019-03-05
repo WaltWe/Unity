ebd1b386df1b10b88745c4c61c61e8d4f5eadca9
WaltWe <walter.weiffenbach@stemhigh.org>
2019-03-05 14:39:41 -0800

    cmmit

---
+++ Iditarod/Assets/Sled.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sled : MonoBehaviour
+{
+    private CharacterController characterController;
+    [SerializeField]
+    private float moveSpeed = 100;
+    [SerializeField]
+    private float turnSpeed = 5f;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.Move(movement * Time.deltaTime * moveSpeed);
+        if (movement.magnitude > 0)
+        {
+            Quaternion newDirection = Quaternion.LookRotation(movement);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        }
+
+    }
+}
